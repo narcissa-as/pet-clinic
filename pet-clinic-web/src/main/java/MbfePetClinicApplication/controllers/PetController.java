a3c3003afc031fd0364ab391b95baf06525035dd
narcissa-as <n.amirsardari@gmail.com>
2022-04-08 19:07:57 +0430

    chapter11-video 28 @ModelAttribute usage for preparing a list to populate a dropdown in controller,for example owner and petType in Pet controller

---
+++ pet-clinic-web/src/main/java/MbfePetClinicApplication/controllers/PetController.java
@@ -0,0 +1,42 @@
+package MbfePetClinicApplication.controllers;
+
+import MbfePetClinicApplication.model.Owner;
+import MbfePetClinicApplication.model.PetType;
+import MbfePetClinicApplication.services.OwnerService;
+import MbfePetClinicApplication.services.PetService;
+import MbfePetClinicApplication.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping()
+public class PetController {
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+    //usage of @ModelAttribute for showing a list in drop down lists
+    @ModelAttribute("types")
+    public Collection<PetType>populatePetTypes(){
+        return petTypeService.findAll();
+    }
+    @ModelAttribute("owner")
+    public Owner populateOwner(@PathVariable Long ownerId){
+        return ownerService.findById(ownerId);
+    }
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+    }
+}
