b80ab08c6dbe5d461a4a81591a85b58bd36a7fa9
narcissa-as <n.amirsardari@gmail.com>
2022-05-02 17:15:55 +0430

    chapter12-video 22 implement vet JSON endpoint

--- pet-clinic-web/src/main/java/MbfePetClinicApplication/controllers/VetController.java
+++ pet-clinic-web/src/main/java/MbfePetClinicApplication/controllers/VetController.java
@@ -1,9 +1,14 @@
 package MbfePetClinicApplication.controllers;
 
+import MbfePetClinicApplication.model.Vet;
 import MbfePetClinicApplication.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -13,9 +18,14 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"vets/index","vet/index.html","vets","vets.html"})
-    public String vetList(Model model){
-        model.addAttribute("vets",vetService.findAll());
+    @RequestMapping({"vets/index", "vet/index.html", "vets", "vets.html"})
+    public String vetList(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
